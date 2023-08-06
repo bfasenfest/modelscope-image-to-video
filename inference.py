c28b623ae5c3c2ea86e0dadd017d5eee75a305fb
bfasenfest <aowbackup@gmail.com>
2023-08-06 17:57:41 -0400

    fix duplicate frames being appended and only average the two intermediate frames

--- inference.py
+++ inference.py
@@ -346,9 +346,9 @@ def inference(
             # Smooth transitions. Suggested by bfasenfest
             if t != 0:  # not the first iteration
                 # average last three frames of previous latent and first three frames of current latent
-                video_latents[-1][:, :, -2:, :, :] = (video_latents[-1][:, :, -2:, :, :] + latents[:, :, :2, :, :]) / 2
+                video_latents[-1][:, :, -1:, :, :] = (video_latents[-1][:, :, -1:, :, :] + latents[:, :, :1, :, :]) / 2
 
-            video_latents.append(latents)
+            video_latents.append(latents[:, :, :-1, :, :])
             init_image = latents[:, :, -1, :, :]
 
         # decode latents to pixel space
